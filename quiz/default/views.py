338e01f415ecf5dda8152a8974c6209af8169a28
f3atur3 <t.ryzhikov@mail.ru>
2023-04-13 20:33:33 +0300

    Add all_quiz

--- quiz/default/views.py
+++ quiz/default/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.core.paginator import Paginator
 from default.models import Quiz
 
 def index(request):
@@ -19,7 +19,27 @@ def index(request):
 
 
 def all_quiz(request):
-    return render(request, "Все_викторины.html")
+    search = request.GET.get('search')
+    if search is None:
+        object_list = Quiz.objects.all()
+    else:
+        object_list = Quiz.objects.filter(title__icontains=search)
+    per_page = 10
+    paginator = Paginator(object_list, per_page)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'quizzes': [ {
+            'picture': page_obj[i].icon,
+            'rating': page_obj[i].rating,
+            'id': page_obj[i].id,
+            "description": page_obj[i].description,
+            "title": page_obj[i].title,
+            "num_complition": page_obj[i].views
+            } for i in range(len(page_obj))]
+    }
+    print(context) 
+    return render(request, "Все_викторины.html", context=context)
 
 
 def about(request):
